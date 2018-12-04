87fee491d7b119b998598a5fa050cdb2d7da9fb0
blessing mutava <blessing@mutava.com>
2018-12-04 17:17:43 +0000

    Added GetGuessAndPrintBack() and PrintIntro()

--- BullCowGame/main.cpp
+++ BullCowGame/main.cpp
@@ -2,21 +2,43 @@
 #include <string>
 
 using namespace std;
-int main() {
 
-	//Introduce game
+void PrintIntro();
+string GetGuessAndPrintBack();
+
+
+//Game entry point
+int main() {
+	
+	PrintIntro();
+
+	//Number of turns loop
+	constexpr int NUMBER_OF_TURNS = 5;
+	for (int count = 1; count <= NUMBER_OF_TURNS; count++)
+	{
+		GetGuessAndPrintBack();
+	}
+	
+	return 0;
+}
+
+//Introduces game
+void PrintIntro() {
 	constexpr int WORD_LENGTH = 5;
 	cout << "Welcome to Bulls and Cows, fun word game.\n";
 	cout << "Can you guess the " << WORD_LENGTH << " letter isogram I'm thinking of?\n";
+	cout << endl;
+	return;
+}
 
+string GetGuessAndPrintBack() {
 	//Get player's input
-	cout << "Enter your guess\n";
+	cout << "Enter your guess: ";
 	string Guess = "";
-	cin >> Guess;
+	getline(cin, Guess);
 
 	//Repeat back the player's guess
-	
-	cout << "Your guess is: " << Guess;
-
-	return 0;
+	cout << "Your guess was: " << Guess << endl;
+	cout << endl;
+	return Guess;
 }
